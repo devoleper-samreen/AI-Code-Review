ba4f0c7dcf15575518188a47f4530116da305fb6
devoleper-samreen <samreenmalik52292@gmail.com>
2025-09-03 10:24:23 +0530

    add connect repo api

--- backend/src/controllers/repo.controller.js
+++ backend/src/controllers/repo.controller.js
@@ -24,3 +24,64 @@ export const getRepos = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch repos" });
   }
 };
+
+export const connectRepo = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { repo } = req.body;
+
+    //Get user from DB for github token
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user || !user.githubToken) {
+      return res.status(401).json({
+        success: false,
+        message: "GitHub token not found",
+      });
+    }
+
+    const [owner, repoName] = repo.split("/");
+
+    //create webhook on github
+    const response = await axios.post(
+      `https://api.github.com/repos/${owner}/${repoName}/hooks`,
+      {
+        name: "web",
+        active: true,
+        events: ["pull_request"],
+        config: {
+          url: `${process.env.WEBHOOK_URL}/github/webhook`,
+          content_type: "json",
+          secret: process.env.GITHUB_WEBHOOK_SECRET,
+        },
+      },
+      {
+        headers: {
+          Authorization: `token ${user.githubToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    //save repo in DB
+    const savedRepo = await prisma.repo.create({
+      data: {
+        userId: userId,
+        repoName: repo,
+        webhookId: response.data.id,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Repo connnected successfully",
+      data: savedRepo,
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({
+      success: false,
+      message: "Failed to connect repo",
+    });
+  }
+};
--- backend/src/routes/repo.route.js
+++ backend/src/routes/repo.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getRepos } from "../controllers/repo.controller.js";
+import { getRepos, connectRepo } from "../controllers/repo.controller.js";
 import { verifyAuth } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.get("/", verifyAuth, getRepos);
+router.post("/connect-repo", verifyAuth, connectRepo);
 
 export default router;
