38acf693021b5709485dd6811b20ae45b574c505
Samreen Malik <samreenmalik52292@gmail.com>
2025-10-16 14:28:12 +0530

    setup pm2

--- backend/src/config/queues.js
+++ backend/src/config/queues.js
@@ -1,9 +1,26 @@
 import { Queue } from "bullmq";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const redisConnection = {
+    url: process.env.REDIS_URL, // single line from Upstash
+    tls: {}, // required for rediss://
+    maxRetriesPerRequest: 1, // prevent endless retry inside requests
+    retryStrategy: (times) => {
+      if (times > 1) return null; // stop after 2 total tries (first + one retry)
+      console.warn("🔁 Retrying Redis connection...");
+      return 2000; // wait 2s before retrying
+    },
+  }
+
+console.log(redisConnection);
+
 
 export const embeddingQueue = new Queue("embedding-queue", {
-  connection: { host: "127.0.0.1", port: 6379 },
+  connection: redisConnection,
 });
 
 export const prQueue = new Queue("pr-review", {
-  connection: { host: "127.0.0.1", port: 6379 },
+  connection: redisConnection,
 });
